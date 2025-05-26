e6276b22691681b8b7b2ddb19da045a892ee0958
RohimNurI <rohimnurislam@gmail.com>
2025-05-26 16:34:56 +0700

    Ubah Package DAO

--- src/main/java/com/clinic/dao/DoctorDAO.java
+++ src/main/java/com/clinic/dao/DoctorDAO.java
@@ -1,7 +1,6 @@
-package main.java.com.clinic.dao;
+package com.clinic.dao;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
-import java.sql.ResultSet;
 import java.sql.SQLException;
 
 import com.clinic.model.Doctor;
